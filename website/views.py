481c4c2e3a92dc9b3c41da0315887331c93954d0
Nishant Kumar <learnlogix@gmail.com>
2022-04-24 02:21:30 +0530

    fotters updates

--- website/views.py
+++ website/views.py
@@ -17,7 +17,14 @@ from .forms import landing_pageleads
     #return render(request,'index.html',{"leads":leads})
 
 def landing_page(request):
-    return render(request,'index.html')
+      if request.method =='POST':
+            leads = landing_pageleads(request.POST)
+            if leads.is_valid():
+                  leads.save()
+            print('===== Information is captured======')
+      else:
+            leads = landing_pageleads()
+      return render(request,'index.html',{"leads":leads})
 
 
 
